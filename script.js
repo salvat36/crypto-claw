20caeba9d285618543601be5f1c3ce65ef01cd88
Ryan <salvat36@gmail.com>
2023-03-19 22:39:53 -0500

    Added welcome message eventlistener

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+
+window.addEventListener("DOMContentLoaded", function() {
+    const welcomeMessages = [
+        { name: "Ryan", message: "Welcome Master Ryan" },
+        { name: "Matt", message: "Eyoo Samsquanch" },
+        { name: "Lee", message: "Poopie Pants in the house" }
+      ];
+      
+      const userName = prompt("Halt! Who goes there?:");
+      
+      let message;
+      const userExists = welcomeMessages.find((user) => user.name === userName);
+      
+      if (userExists) {
+        message = userExists.message;
+      } else {
+        message = `Welcome ${userName}!`;
+        welcomeMessages.push({ name: userName, message });
+      }
+      
+      const welcomeMessageElement = document.getElementById("welcome-message");
+      welcomeMessageElement.innerHTML = message;
+  });
